4da49c70ceb33954868c1bf6bf703f2dccc7e559
Alejandro <alejodiscovery20@gmail.com>
2019-03-03 15:48:38 -0500

    updated files

--- scripts/velocity_hub.py
+++ scripts/velocity_hub.py
@@ -2,35 +2,52 @@
 
 """
 How to test:
-    rostopic pub -r 10 /mardan/velocity geometry_msgs/Twist  '{linear:  {x: 0.1, y: 0.0, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}'
+    rostopic pub -r 10 /mardan/velocity geometry_msgs/Twist  '{linear:  {x: 0, y: 0.0, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 100.0}}'
 """
 
 import mardan_msgs.srv as msrv
 import rospy
+import smbus
 
 from geometry_msgs.msg import Twist
 
-# Global variables
-subscriber = None
-i2cService = None
-
 i2cServiceName = 'mardan/i2c'
+
+i2c_adress = 0x03
+
+i2cBus = None
+
+
+def intToByteArray(value):
+    result = []
+    intBytes = 2
+    mask = 0xFF
+
+    for i in range(0, intBytes):
+        result.insert(0, value & mask)
+        value >>= 8
+
+    return result
 
 
 def subscriberCallback(msg):
-    print('message received: linear: {0} angular: {1}'.format(msg.linear.x, msg.angular.x))
+    global i2cBus
+    print('message received: linear: {0} angular: {1}'.format(msg.linear.x, msg.angular.z))
     
-    # TODO: compute left and right motor speeds and send through the I2C service proxy
+    var_r = intToByteArray(int(msg.linear.x + msg.angular.z))
+    var_l = intToByteArray(int(msg.linear.x - msg.angular.z))
+    
+    data = var_r + var_l
+    print('data: {0}'.format(str(data)))
+    
+    i2cBus.write_i2c_block_data(i2c_adress, 0, data)
     
 
 if __name__ == '__main__':
     
-    rospy.init_node('velocity_hub')
+    i2cBus = smbus.SMBus(1)
     
-    rospy.loginfo('waiting for service [{0}] to become available...'.format(i2cServiceName))
-    rospy.wait_for_service(i2cServiceName)
-    i2cService = rospy.ServiceProxy(i2cServiceName, msrv.i2c)
-    rospy.loginfo('service [{0}] available'.format(i2cServiceName))
+    rospy.init_node('velocity_hub')
     
     subscriber = rospy.Subscriber('mardan/velocity', Twist, subscriberCallback)
     
