87bcb61d2baec717e450dc0c46a4b4da65f4cd05
Juan David Adarve <juanda0718@gmail.com>
2023-08-20 10:10:39 -0500

    Refactor node structue.

---
+++ src/ros/mardan/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# see https://github.com/daniel-robotics/ros_python_pkg for more details
+
+from os.path import dirname, abspath, basename
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+setup_args = generate_distutils_setup(
+    packages=[basename(dirname(abspath(__file__)))],
+    package_dir={'': 'src'},
+)
+
+setup(**setup_args)
